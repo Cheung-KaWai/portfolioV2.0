7a457958fd5fe2cf0bb3e3ee0c83546c5c36e64b
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-06-19 00:22:40 +0200

    Add hamburger and cross icons

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -2,17 +2,24 @@ import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 import { gsap } from "gsap";
 import Link from "next/link";
+import Hamburger from "./Hamburger";
 
 export default function Menu() {
   const [toggle, setToggle] = useState(false);
 
   const menuRef = useRef();
   const menuLinks = useRef();
+  const hamburger = useRef();
+  const cross = useRef();
   const tl = useRef();
 
   useEffect(() => {
     tl.current = gsap
       .timeline({ paused: true })
+      .to(hamburger.current, {
+        opacity: 0,
+        duration: 0.2,
+      })
       .to(menuRef.current, {
         width: "200vw",
         height: "200vw",
@@ -23,10 +30,14 @@ export default function Menu() {
         ease: "slow(0.7, 0.7, false)",
       })
       .to(menuRef.current, {
-        backgroundColor: "rgb(1, 1, 1)",
+        backgroundColor: "rgb(20, 20, 20)",
       })
       .to(menuLinks.current, {
         visibility: "visible",
+      })
+      .to(cross.current, {
+        opacity: 1,
+        duration: 0.2,
       });
   }, []);
 
@@ -41,6 +52,7 @@ export default function Menu() {
   return (
     <>
       <NavContainer ref={menuRef} onClick={toggleMenu}>
+        <Hamburger hamburger={hamburger} cross={cross} />
         <NavContent ref={menuLinks}>
           <NavItem>
             <Link href="#home">
@@ -80,22 +92,24 @@ const NavContainer = styled.nav`
   transform-origin: center;
   overflow: hidden;
   z-index: 5;
+  box-shadow: 0 0 15px 10px rgba(0, 0, 0, 0.3);
 `;
 
 const NavContent = styled.ul`
   list-style: none;
-  padding: 5rem 3rem;
+  padding: 10rem 5rem;
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 2rem;
   position: fixed;
   left: 5rem;
-  z-index: 10;
   visibility: hidden;
 `;
 
 const NavItem = styled.li`
   padding: 1rem 0;
-  font-size: 1.6rem;
-  z-index: 10;
+
+  a {
+    font-size: 2.4rem;
+  }
 `;
